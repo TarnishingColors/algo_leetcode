1707e3e373e5f9bf42efbbf7cc62285e7479e134
agabitashvili <alex.gabitas@gmail.com>
2022-10-05 20:55:38 +0200

    add 3 solution

---
+++ leetcode/m_3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        i = j = 0
+        max_len = 0
+        while i < len(s):
+            if len(set(s[i:j])) == len(s[i:j]):
+                max_len = max(j - i, max_len)
+                if j >= len(s):
+                    break
+                j += 1
+            else:
+                i += 1
+        return max_len
---
+++ test/m_3.py
@@ -0,0 +1,23 @@
+from leetcode.m_3 import Solution
+
+
+s = Solution()
+
+
+def test_case1():
+    print(s.lengthOfLongestSubstring('abcabcbb'))
+    # assert s.lengthOfLongestSubstring('abcabcbb') == 3
+
+
+def test_case2():
+    assert s.lengthOfLongestSubstring('bbbbb') == 1
+
+
+def test_case3():
+    assert s.lengthOfLongestSubstring('pwwkew') == 3
+
+
+if __name__ == "__main__":
+    test_case1()
+    test_case2()
+    test_case3()
